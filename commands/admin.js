953d315cc9f9e768a8009a71fe2ebf0a4a16a9a4
Nathan Laney <nathaniel.laney@gmail.com>
2024-06-30 17:33:34 -0400

    Admin commands

---
+++ commands/admin.js
@@ -0,0 +1,293 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const profileModel = require("../models/profileSchema");
+const characterModel = require("../models/characterSchema");
+const calculateGainedGPAndLevel = require("../experienceTable");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("admin")
+    .setDescription(
+      "Administration commands for server owners to manage XP of others."
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+    .addSubcommandGroup((group) =>
+      group
+        .setName("xp")
+        .setDescription("Manage XP.")
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("addbank")
+            .setDescription("Add XP to the bank.")
+            .addUserOption((option) =>
+              option
+                .setName("user")
+                .setDescription("The user to add XP to.")
+                .setRequired(true)
+            )
+            .addIntegerOption((option) =>
+              option
+                .setName("amount")
+                .setDescription("The amount of XP to add.")
+                .setRequired(true)
+                .setMinValue(1)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("mission")
+                .setDescription("The mission from which you received XP.")
+                .setRequired(true)
+            )
+        )
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("removebank")
+            .setDescription("Remove XP from the bank.")
+            .addUserOption((option) =>
+              option
+                .setName("user")
+                .setDescription("The user to remove XP from.")
+                .setRequired(true)
+            )
+            .addIntegerOption((option) =>
+              option
+                .setName("amount")
+                .setDescription("The amount of XP to remove.")
+                .setRequired(true)
+                .setMinValue(1)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("mission")
+                .setDescription("The mission you want removed (if any).")
+            )
+        )
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("addcharacter")
+            .setDescription("Add XP to a character.")
+            .addUserOption((option) =>
+              option
+                .setName("user")
+                .setDescription("The user to add XP to.")
+                .setRequired(true)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("target")
+                .setDescription("The character to add XP to.")
+                .setRequired(true)
+            )
+            .addIntegerOption((option) =>
+              option
+                .setName("amount")
+                .setDescription("The amount of XP to add.")
+                .setRequired(true)
+                .setMinValue(1)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("mission")
+                .setDescription("The mission from which you received XP.")
+                .setRequired(true)
+            )
+        )
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("removecharacter")
+            .setDescription("Remove XP from a character.")
+            .addUserOption((option) =>
+              option
+                .setName("user")
+                .setDescription("The user to remove XP from.")
+                .setRequired(true)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("target")
+                .setDescription("The character to remove XP from.")
+                .setRequired(true)
+            )
+            .addIntegerOption((option) =>
+              option
+                .setName("amount")
+                .setDescription("The amount of XP to remove.")
+                .setRequired(true)
+                .setMinValue(1)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("mission")
+                .setDescription("The mission you want removed (if any).")
+            )
+        )
+    )
+    .addSubcommandGroup((group) =>
+      group
+        .setName("character")
+        .setDescription("Character management commands.")
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("add")
+            .setDescription("Create a new character.")
+            .addUserOption((option) =>
+              option
+                .setName("user")
+                .setDescription("The user to create the character for.")
+                .setRequired(true)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("character_name")
+
+                .setName("character_name")
+                .setDescription("The name of the character.")
+                .setRequired(true)
+            )
+        )
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("remove")
+            .setDescription("Delete a character.")
+            .addUserOption((option) =>
+              option
+                .setName("user")
+                .setDescription("The user to delete the character for.")
+                .setRequired(true)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("character_name")
+                .setDescription("The name of the character.")
+                .setRequired(true)
+            )
+        )
+    ),
+  async execute(interaction) {
+    await interaction.deferReply();
+    const group = interaction.options.getSubcommandGroup();
+    const subcommand = interaction.options.getSubcommand();
+
+    const targetUser = interaction.options.getUser("user");
+    const amount = interaction.options.getInteger("amount");
+    const mission = interaction.options.getString("mission");
+    const characterName =
+      interaction.options.getString("character_name") ||
+      interaction.options.getString("target");
+
+    if (group === "xp") {
+      if (subcommand === "addbank" || subcommand === "removebank") {
+        const profile = await profileModel.findOne({ userId: targetUser.id });
+        if (!profile) {
+          return interaction.editReply("This profile does not exist!");
+        }
+
+        const update = {
+          $inc: { experience: subcommand === "addbank" ? amount : -amount },
+          ...(subcommand === "addbank"
+            ? { $push: { missions: mission } }
+            : { $pull: { missions: mission } }),
+        };
+
+        const result = await profileModel.findOneAndUpdate(
+          { userId: targetUser.id },
+          update,
+          { new: true }
+        );
+
+        if (!result) {
+          return interaction.editReply("This profile does not exist!");
+        }
+
+        return interaction.editReply(
+          `${subcommand === "addbank" ? "Added" : "Removed"} ${amount} XP ${
+            subcommand === "addbank" ? "to" : "from"
+          } ${targetUser.username}'s bank from ${mission}.`
+        );
+      }
+
+      if (subcommand === "addcharacter" || subcommand === "removecharacter") {
+        const character = await characterModel.findOne({
+          ownerId: targetUser.id,
+          characterName: characterName,
+        });
+        if (!character) {
+          return interaction.editReply("This character does not exist!");
+        }
+
+        const oldExperience = character.experience;
+
+        const update = {
+          $inc: {
+            experience: subcommand === "addcharacter" ? amount : -amount,
+          },
+          ...(subcommand === "addcharacter"
+            ? { $push: { missions: mission } }
+            : { $pull: { missions: mission } }),
+        };
+
+        const result = await characterModel.findOneAndUpdate(
+          {
+            ownerId: targetUser.id,
+            characterName: characterName,
+          },
+          update,
+          { new: true }
+        );
+
+        if (!result) {
+          return interaction.editReply("This character does not exist!");
+        }
+
+        const newExperience = result.experience;
+        const experienceGained = newExperience - oldExperience;
+        const earnings = calculateGainedGPAndLevel(
+          oldExperience,
+          newExperience
+        );
+
+        return interaction.editReply(
+          `${
+            subcommand === "addcharacter" ? "Added" : "Removed"
+          } **${amount}** XP ${
+            subcommand === "addcharacter" ? "to" : "from"
+          } **${characterName}** from \`${mission}\`. \n**${characterName}** now has a total of **${newExperience}** XP, ${
+            subcommand === "addcharacter" ? "gains" : "loses"
+          } **${earnings.gpGained}** GP and is now level **${
+            earnings.characterLevel
+          }**.`
+        );
+      }
+    }
+
+    if (group === "character") {
+      if (subcommand === "add") {
+        const newCharacter = new characterModel({
+          ownerId: targetUser.id,
+          characterName: characterName,
+          experience: 0,
+          missions: [],
+        });
+
+        await newCharacter.save();
+        return interaction.editReply(
+          `Character **${characterName}** has been created for ${targetUser.username}.`
+        );
+      }
+
+      if (subcommand === "remove") {
+        const character = await characterModel.findOneAndDelete({
+          ownerId: targetUser.id,
+          characterName: characterName,
+        });
+
+        if (!character) {
+          return interaction.editReply("This character does not exist!");
+        }
+
+        return interaction.editReply(
+          `Character **${characterName}** has been deleted for ${targetUser.username}.`
+        );
+      }
+    }
+  },
+};
--- commands/xp.js
+++ commands/xp.js
@@ -7,151 +7,191 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("xp")
     .setDescription("Manage your XP.")
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("character")
-        .setDescription("Add XP to a character.")
-        .addStringOption((option) =>
-          option
-            .setName("target")
-            .setDescription("The character to add XP to.")
-            .setRequired(true)
+    .addSubcommandGroup((group) =>
+      group
+        .setName("add")
+        .setDescription("Add XP.")
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("character")
+            .setDescription("Add XP to a character.")
+            .addStringOption((option) =>
+              option
+                .setName("target")
+                .setDescription("The character to add XP to.")
+                .setRequired(true)
+            )
+            .addIntegerOption((option) =>
+              option
+                .setName("amount")
+                .setDescription("The amount of XP to add.")
+                .setRequired(true)
+                .setMinValue(1)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("mission")
+                .setDescription("The mission from which you received XP.")
+                .setRequired(true)
+            )
         )
-        .addIntegerOption((option) =>
-          option
-            .setName("amount")
-            .setDescription("The amount of XP to add.")
-            .setRequired(true)
-            .setMinValue(1)
-        )
-        .addStringOption((option) =>
-          option
-            .setName("mission")
-            .setDescription("The mission from which you recieved XP.")
-            .setRequired(true)
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("bank")
+            .setDescription("Add XP to your bank.")
+            .addIntegerOption((option) =>
+              option
+                .setName("amount")
+                .setDescription("The amount of XP to add.")
+                .setRequired(true)
+                .setMinValue(1)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("mission")
+                .setDescription("The mission from which you received XP.")
+                .setRequired(true)
+            )
         )
     )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName("bank")
-        .setDescription("Bank this XP.")
-        .addIntegerOption((option) =>
-          option
-            .setName("amount")
-            .setDescription("The amount of XP to add.")
-            .setRequired(true)
-            .setMinValue(1)
+    .addSubcommandGroup((group) =>
+      group
+        .setName("remove")
+        .setDescription("Remove XP.")
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("character")
+            .setDescription("Remove XP from a character.")
+            .addStringOption((option) =>
+              option
+                .setName("target")
+                .setDescription("The character to remove XP from.")
+                .setRequired(true)
+            )
+            .addIntegerOption((option) =>
+              option
+                .setName("amount")
+                .setDescription("The amount of XP to remove.")
+                .setRequired(true)
+                .setMinValue(1)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("mission")
+                .setDescription("The mission from which you received XP.")
+                .setRequired(true)
+            )
         )
-        .addStringOption((option) =>
-          option
-            .setName("mission")
-            .setDescription("The mission from which you recieved XP.")
-            .setRequired(true)
+        .addSubcommand((subcommand) =>
+          subcommand
+            .setName("bank")
+            .setDescription("Remove XP from your bank.")
+            .addIntegerOption((option) =>
+              option
+                .setName("amount")
+                .setDescription("The amount of XP to remove.")
+                .setRequired(true)
+                .setMinValue(1)
+            )
+            .addStringOption((option) =>
+              option
+                .setName("mission")
+                .setDescription("The mission from which you received XP.")
+                .setRequired(true)
+            )
         )
     ),
   async execute(interaction) {
     await interaction.deferReply();
+    const group = interaction.options.getSubcommandGroup();
     const subcommand = interaction.options.getSubcommand();
+
+    const user = interaction.user.id;
+    const amount = interaction.options.getInteger("amount");
+    const mission = interaction.options.getString("mission");
+
     if (subcommand === "bank") {
       const target = interaction.member.nickname;
-      const user = interaction.user.id;
-      const amount = interaction.options.getInteger("amount");
-      const mission = interaction.options.getString("mission");
-      console.log(
-        "Adding " + amount + " XP to " + user + " bank from " + mission + "."
-      );
-
-      // console.log("Amount: ", amount);
       const profile = await profileModel.findOne({ userId: user });
       if (!profile) {
-        return interaction.editReply("This character does not exist!");
+        return interaction.editReply("This profile does not exist!");
       }
-      // console.log("Profile: ", profile);
-      // console.log("Experience before update: ", profile.experience);
-      const oldExperience = profile.experience;
+
+      let update;
+      if (group === "add") {
+        update = { $inc: { experience: amount }, $push: { missions: mission } };
+      } else if (group === "remove") {
+        update = {
+          $inc: { experience: -amount },
+          $pull: { missions: mission },
+        };
+      }
 
       const result = await profileModel.findOneAndUpdate(
-        {
-          userId: user,
-        },
-        {
-          $inc: {
-            experience: amount,
-          },
-          $push: {
-            missions: mission,
-          },
-        },
-        {
-          new: true, // This option returns the modified document rather than the original.
-        }
+        { userId: user },
+        update,
+        { new: true }
       );
-      if (!result) {
-        return interaction.editReply("This character does not exist!");
-      }
-      const newExperience = result.experience;
 
-      // if (!result) {
-      //     return interaction.editReply("This character does not exist!.");
-      // }
+      if (!result) {
+        return interaction.editReply("This profile does not exist!");
+      }
+
+      const newExperience = result.experience;
       return interaction.editReply(
-        `Added ${amount} XP to ${target}'s bank from ${mission}.`
+        `${group === "add" ? "Added" : "Removed"} ${amount} XP ${
+          group === "add" ? "to" : "from"
+        } ${target}'s bank from ${mission}.`
       );
     }
+
     if (subcommand === "character") {
       const target = interaction.options.getString("target");
-      const user = interaction.user;
-      const amount = interaction.options.getInteger("amount");
-      const mission = interaction.options.getString("mission");
-      console.log(
-        "Adding " +
-          amount +
-          " XP to " +
-          user.id +
-          " character " +
-          target +
-          " from " +
-          mission +
-          "."
-      );
-
       const character = await characterModel.findOne({
-        ownerId: user.id,
+        ownerId: user,
         characterName: target,
       });
       if (!character) {
         return interaction.editReply("This character does not exist!");
       }
-      // console.log("Profile: ", profile);
-      // console.log("Character before update: ", character);
+
       const oldExperience = character.experience;
+
+      let update;
+      if (group === "add") {
+        update = { $inc: { experience: amount }, $push: { missions: mission } };
+      } else if (group === "remove") {
+        update = {
+          $inc: { experience: -amount },
+          $pull: { missions: mission },
+        };
+      }
+
       const result = await characterModel.findOneAndUpdate(
         {
-          ownerId: user.id,
+          ownerId: user,
           characterName: target,
         },
-        {
-          $inc: {
-            experience: amount,
-          },
-          $push: {
-            missions: mission,
-          },
-        },
-        {
-          new: true, // This option returns the modified document rather than the original.
-        }
+        update,
+        { new: true }
       );
+
       if (!result) {
         return interaction.editReply("This character does not exist!");
       }
+
       const newExperience = result.experience;
-      // console.log("Character after update: ", result);
       const experienceGained = newExperience - oldExperience;
-      // console.log("Experience gained: ", experienceGained);
       const earnings = calculateGainedGPAndLevel(oldExperience, newExperience);
+
       return interaction.editReply(
-        `Added **${amount}** XP to **${target}** from \`${mission}\`. \n**${target}** now has a total of **${newExperience}** XP, gains **${earnings.gpGained}** GP and is now level **${earnings.characterLevel}**.`
+        `${group === "add" ? "Added" : "Removed"} **${amount}** XP ${
+          group === "add" ? "to" : "from"
+        } **${target}** from \`${mission}\`. \n**${target}** now has a total of **${newExperience}** XP, ${
+          group === "add" ? "gains" : "loses"
+        } **${earnings.gpGained}** GP and is now level **${
+          earnings.characterLevel
+        }**.`
       );
     }
   },
